cb75a547ddfa7cd2c8750ecda33d2e4f07b4e2ae
zhaomm <291732699@qq.com>
2017-01-17 15:14:10 +0800

    == New List ==  * Update :Gitblit 配置文件

--- README.md
+++
@@ -1,3 +0,0 @@
-**SoftWare Configuration specification**
-
-
---
+++ Start up.bat
@@ -0,0 +1,20 @@
+
+start "" "D:\Program Files\Git\git-bash.exe"
+
+start "" "D:\Software\Pro\eclipse\eclipse.exe"
+
+start "" "C:\Program Files\VanDyke Software\SecureCRT\SecureCRT.exe"
+
+start "" "D:\Program Files\AllWinnerTech\PhoenixSuit\PhoenixSuit.exe"
+
+
+
+#start "" "D:\Program Files\Kingsoft\Power Word 2016\2016.3.0.0248\PowerWord.exe"
+
+#start "" "C:\Users\Administrator\AppData\Local\PomoDoneApp\app-1.5.877\PomoDoneApp.exe"
+
+start "" "D:\Program Files\Mozilla Firefox\firefox.exe"
+
+start "" "D:\Program Files\GPSoftware\Directory Opus\dopus.exe"
+
+
